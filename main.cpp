c7c71a767c7ae2889e7ba5d59789a3c69a5edde2
mikplavsky <mikhail.plavskiy@quest.com>
2020-08-17 00:38:01 +0300

    nice lambda capturing

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <array>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int n,q;
+    cin >> n >> q;
+    
+    vector< vector<int> > arr;
+    
+    for (int i = 0; i < n ; ++i ) {
+
+        int k;
+        cin >> k; 
+
+        auto c = vector<int>();
+
+        for (int j = 0; j < k ; ++j ) {
+
+            int el;  
+            cin >> el;
+
+            c.push_back(el);
+
+        }
+
+        arr.push_back(c);
+
+    }
+
+    
+    vector< array<int,2> > qs;
+
+    for (int i = 0; i < q ; ++i){
+
+        array<int,2> query;
+        cin >> query[0] >> query[1];
+
+        qs.push_back(query);
+
+    }
+
+    for_each(qs.begin(),qs.end(),[&arr](auto q){
+            cout << arr[q[0]][q[1]] << std::endl; });
+
+    return 0;
+
+}
