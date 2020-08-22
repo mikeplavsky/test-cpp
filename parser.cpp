be1e2da99fa2413af770275f1871bb01f2f22bbc
mikplavsky <mikhail.plavskiy@quest.com>
2020-08-23 02:27:32 +0300

    added empty attributes reader

--- parser.cpp
+++ parser.cpp
@@ -12,33 +12,47 @@ struct Tag {
     map<string,string> attrs;
 };
 
+map<string,string> parse_attrs(){
+
+    map<string,string> res;
+    bool attr = true;
+
+    for (;;) {
+
+        string l;
+        cin >> l;
+
+        attr = !attr;
+
+        auto e = l.rbegin();
+
+        if (*e == '>') {
+            break;
+        } 
+
+    }
+
+    return res;
+
+}
+
 std::pair<Tag,bool> parse_tag() {
 
     Tag tag;
     bool closed = false;
 
-    for (;;){
+    string t;
+    cin >> t;
 
-        string t;
-        cin >> t;
+    auto b = t.begin();
+    tag.name = t.substr(1);
 
-        auto b = t.begin();
-        auto e = t.rbegin();
-
-        if (*b == '<'){
-
-            tag.name = t.substr(1);
-
-            if (*next(b) == '/') {
-                tag.name = t.substr(2,t.length()-3);
-                closed = true;
-            }
-
-        }
-
-        if (*e == '>') {
-            break;
-        } 
+    if (*next(b) == '/') {
+        tag.name = t.substr(2,t.length()-3);
+        closed = true;
+    }
+    else {
+        tag.attrs = parse_attrs();
     }
 
     return make_pair(tag,closed);
