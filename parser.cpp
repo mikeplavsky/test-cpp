5f2dccb8d8b7f84ce1439266ef2898e58c85de1e
mikplavsky <mikhail.plavskiy@quest.com>
2020-08-23 21:11:44 +0300

    reading attributes

--- parser.cpp
+++ parser.cpp
@@ -18,16 +18,35 @@ struct Tag {
 map<string,string> parse_attrs(){
 
     map<string,string> res;
+
     bool attr = true;
+    string attr_name;
 
     for (;;) {
 
         string l;
         cin >> l;
 
-        attr = !attr;
-
         auto e = l.rbegin();
+
+        if (not attr) {
+
+            string v = l.substr(1,l.length() - 2);
+
+            if (v.back() == '\"') {
+                v = v.substr(0,v.length() - 1);
+            }
+
+            res[attr_name] = v;
+            attr = true;
+
+        } 
+        else if (attr and *e == '=') {
+            attr = false;
+        }
+        else if (attr) {
+            attr_name = l;
+        }
 
         if (*e == '>') {
             break;
@@ -92,10 +111,12 @@ int main() {
     }
 
     for (auto [a,b]: tags) {
+
         cout 
             << a
             << ' ' << (b -> parent? b -> parent  -> name: "")
             << "\n";
+
     }
 
     for (int i = 0; i < q; ++i) {
